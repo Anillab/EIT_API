437daa4385922c88502397297af4608763664084
Anillab <anillabusolo@gmail.com>
2020-01-19 18:37:11 +0000

    defined  a createServer function

--- index.js
+++ index.js
@@ -1,17 +1,29 @@
 // Dependencies
 const http=require('http');
-
-http.createServer((request,response)=>{
-  const { headers,method,url } =request;
-  let body=[];
-  request.on('error',(err)=>{
-    // catch the error
-    console.error(err);
-  }).on('data',(chunk)=>{
-    body.push(chunk);
-  }).on('end',()=>{
-    body=Buffer.concat(body).toString()
-  });
-  // Activates the server to listen on a port
-}).listen(8000);
 const url=require('url');
+let httpServer= http.createServer((request,response)=>{
+  let parsedUrl=url.parse(request.url,true);
+  // Get the path
+  let path=parsedUrl.pathname;
+  let trimmedPath=path.replace(/^\/+|\/+$/g,'');
+  // Get the querystring as an object
+  let queryStringObject=parsedUrl.query;
+  // Get the Method
+  let method=request.method.toLowerCase();
+  // Get the headers of the Object
+  let headers=request.headers;
+  // get the payloads;
+ let decoder=new StringDecoder('utf-8');
+ var buffer='';
+ request.on('error',(error)=>{
+   console.error(error);
+ }).on('data',(data)=>{
+   buffer +=decoder.write(data);
+ }).end('end',()=>{
+   buffer +=decoder.end()
+   // choose where the request should go
+ })
+});
+httpServer.listen(8000,()=>{
+  console.log('The server is listening on port',8000);
+})
